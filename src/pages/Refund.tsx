8849f7ec4da3f595f5973a755d47f4bd648297cb
Chandilene <chandileneborges@gmail.com>
2026-04-15 15:10:45 -0300

    feat: enviando dados de solicitação para api e guardando token no cabeçalho da requisição

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import { useNavigate, useParams } from "react-router";
 import { z, ZodError } from "zod";
 
+import { api } from "../services/api";
+import { AxiosError } from "axios";
+
 import fileSvg from "../assets/file.svg";
 import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories";
 
@@ -30,7 +33,7 @@ export function Refund() {
   const navigate = useNavigate();
   const params = useParams<{ id: string }>();
 
-  function onSubmit(e: React.SubmitEvent) {
+  async function onSubmit(e: React.SubmitEvent) {
     e.preventDefault();
     if (params.id) {
       return navigate(-1);
@@ -45,13 +48,21 @@ export function Refund() {
         amount: amount.replace(",", "."),
       });
 
-      // console.log(data);
+      await api.post("/refunds", {
+        ...data,
+        filename: "sufuivawerianvanodvmelkit.png",
+      });
+
       navigate("/confirm", { state: { fromSubmit: true } });
     } catch (error) {
       console.log(error);
 
       if (error instanceof ZodError) {
         return alert(error.issues[0].message);
+      }
+
+      if (error instanceof AxiosError) {
+        return alert(error.response?.data.message);
       }
 
       alert("Não foi possivel criar a solicitação");
